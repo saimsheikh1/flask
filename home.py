45bed3eebcd1a77a1333d896f9a2fba5b9a3801d
saimsheikh1 <78775621+saimsheikh1@users.noreply.github.com>
2022-03-28 17:30:37 -0700

    Create home.py

---
+++ home.py
@@ -0,0 +1,25 @@
+from flask import Flask
+
+my_obj = Flask (__name__)
+@my_obj.route("/")
+def home():
+    name  = {'username': 'Lisa'}
+    city_names = ['Paris', 'London', 'Rome', 'Tahiti']
+    return '''
+    <html>
+    <body>
+        <h1>
+        Welcome '''+ name['username'] + ''' </h1>
+        <div>
+            <a href ="https://www.google.com">not google</a>
+        </div>
+        <ul>
+            <li> '''+city_names[0]+''' </li>
+            <li> '''+city_names[1]+''' </li>
+            <li> '''+city_names[2]+''' </li>
+            <li> '''+city_names[3]+''' </li>
+        </ul>
+    </body>
+    </html>
+    '''
+my_obj.run()
