23a0261a8de734a55154acac2c46a925d03f2196
saimsheikh1 <78775621+saimsheikh1@users.noreply.github.com>
2022-03-28 17:44:02 -0700

    Update home.py

--- home.py
+++ home.py
@@ -12,7 +12,7 @@ def home():
         <h1>
         Welcome '''+ name['username'] + ''' </h1>
         <div>
-            <a href ="https://www.google.com">not google</a>
+            <a href ="https://www.google.com">notgoogle</a>
         </div>
         <ul>
             <li> '''+city_names[0]+''' </li>
