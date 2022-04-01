5dbb8ecfa3c7d77a7e969a1dc30db315531370c5
saimsheikh1 <78775621+saimsheikh1@users.noreply.github.com>
2022-03-31 18:11:00 -0700

    Add files via upload

---
+++ Flask/app/routes.py
@@ -0,0 +1,27 @@
+from app import my_obj
+
+from flask import render_template, flash, Flask, redirect, url_for, request
+
+
+#my_obj = Flask (__name__)      running this line twice, first init
+name  =  'Lisa'
+city_names = ['Paris', 'London', 'Rome', 'Tahiti']
+#city_names = list, city = string field 
+
+
+@my_obj.route("/", methods=['GET', 'POST'])
+def home():
+    
+    if (request.method=='POST'):
+        flash(request.form["cityName"])
+        return redirect(url_for('home'))
+    return render_template("home.html", name = name, city_names= city_names)
+    
+if __name__ == "__main__":
+    my_obj.run(debug=True)
+    #app.run(port= 80)
+
+
+#Christian Hernandez helped me
+
+
