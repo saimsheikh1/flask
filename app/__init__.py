f656a27d34e0082879eb6eb6bc226f22fa0b0206
saimsheikh1 <78775621+saimsheikh1@users.noreply.github.com>
2022-03-31 18:16:22 -0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
 
-my_obj = Flask(__name__)
+myobj = Flask(__name__)
 
-my_obj.config.from_mapping(
+myobj.config.from_mapping(
     SECRET_KEY = 'you-will-never-guess')
 
 from app import routes
